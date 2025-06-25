8ef238723a9b0f4603de05b5bdbd7011fd1997b2
Your Name <you@example.com>
2025-06-26 00:30:18 +0530

    new changes

--- vite-project/src/context/AuthContext.jsx
+++ vite-project/src/context/AuthContext.jsx
@@ -1,21 +1,21 @@
 
 
-import React from 'react'
-import { createContext } from 'react'
-export const authDataContext=createContext()
-const serverUrl="https://echoai-2.onrender.com"
+// import React from 'react'
+// import { createContext } from 'react'
+// export const authDataContext=createContext()
+// const serverUrl="https://echoai-2.onrender.com"
 
 
 
-const authcontext = ({children}) => {
-    let value={serverUrl}
-  return (
-    <div>
-        <authDataContext.Provider value={value}>
-           {children}
-        </authDataContext.Provider>
-    </div>
-  )
-}
+// const authcontext = ({children}) => {
+//     let value={serverUrl}
+//   return (
+//     <div>
+//         <authDataContext.Provider value={value}>
+//            {children}
+//         </authDataContext.Provider>
+//     </div>
+//   )
+// }
 
-export default authcontext
+// export default authcontext
