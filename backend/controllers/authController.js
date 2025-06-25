8a09426af218acf727e3b5acbc929371f104e61f
Snehal Mehra <snehalmehra017@gmail.com>
2025-06-25 19:35:03 +0530

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -69,8 +69,8 @@ export const login=async(req,res)=>{
         res.cookie("token",token,{
         httpOnly:true,
         maxAge:7*24*60*60*1000,
-        sameSite: "Lax", // ✅ Safe for same-origin requests
-         secure: false,   // ✅ Don't use HTTPS in development
+        secure: true,        
+       sameSite: "None" 
         })
         return res.status(201).json({
             message:"User login successfully",
